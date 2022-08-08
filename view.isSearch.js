7c3f03b1cff5993718d9f0ec1c1b417c6585d845
dataunlimitedgroup <110808287+dataunlimitedgroup@users.noreply.github.com>
2022-08-08 13:43:04 +0700

    Create view.isSearch.js

---
+++ view.isSearch.js
@@ -0,0 +1,7 @@
+/* Script isSearch
+Owner = Data Unlimited
+diBuat Oleh = https://bit.ly/3vMYLK9 */
+$(function(){if($("#indeks").length){var o=$("#indeks"),t=$("#indeks").offset().top,i=$("#indeks").height();$(window).scroll(function(){var s=$("#blog-pager").offset().top-i-0,f=$(window).scrollTop(); if(f>t?o.css({position:"fixed",top:36}):o.css("position","static"),f>s){var n=s-f;o.css({top:n})}})}});
+/* Script isSearch
+Owner = Data Unlimited
+diBuat Oleh = https://bit.ly/3vMYLK9 */
